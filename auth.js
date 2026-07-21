807a42729cd082e0dee0295663b2fc9666d7f7f0
S-Matrix <sagaralient@gmail.com>
2026-07-21 18:57:19 +0600

    Implement user authentication functions
    Added user authentication functions for registration, login, and logout.

--- auth.js
+++ auth.js
@@ -1,4 +1,33 @@
 import { auth } from "./firebase.js";
 
-console.log("auth.js loaded");
-console.log("Firebase Auth =", auth);
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+window.registerUser = async function(email, password){
+
+    return await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+    );
+
+}
+
+window.loginUser = async function(email,password){
+
+    return await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+    );
+
+}
+
+window.logoutUser = async function(){
+
+    await signOut(auth);
+
+}
